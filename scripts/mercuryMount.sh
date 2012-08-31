70835b7619767e9588587a5d7e84e547d40c4927
John Hamelink <john@johnhamelink.com>
2012-08-31 09:21:59 +0100

    Added downloads directory to my mount script.

--- scripts/mercuryMount.sh
+++ scripts/mercuryMount.sh
@@ -1,3 +1,4 @@
-sudo mount -t smbfs //mercury.local/Music ~/Music -o username=guest,password=guest,uid=john,gid=audio
-sudo mount -t smbfs //mercury.local/Movies ~/Videos/Movies -o username=guest,password=guest,uid=john,gid=john
-sudo mount -t smbfs //mercury.local/TV ~/Videos/TV -o username=guest,password=guest,uid=john,gid=john
+sudo mount -t smbfs //mercury.local/Music ~/music -o username=guest,password=guest,uid=john,gid=audio
+sudo mount -t smbfs //mercury.local/Movies ~/videos/Movies -o username=guest,password=guest,uid=john,gid=john
+sudo mount -t smbfs //mercury.local/TV ~/videos/TV -o username=guest,password=guest,uid=john,gid=john
+sudo mount -t smbfs //mercury.local/Download ~/downloads -o username=guest,password=guest,uid=john,gid=john
