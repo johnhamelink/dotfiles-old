59dc4e3a76a3965439a2634bb0eb565d81ffece9
John Hamelink <hello@farmer.io>
2014-09-09 23:06:48 +0100

    Added handy whiteboardify command

--- zsh/Linux.sh
+++ zsh/Linux.sh
@@ -5,3 +5,7 @@ alias pacman="pacaur"
 alias gksu="gksudo"
 alias open="xdg-open"
 export BROWSER="chromium"
+
+whiteboardify (){
+    convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 3x1 -channel RBG -level 60%,91%,0.1 "$2"
+}
