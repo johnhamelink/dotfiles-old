acde4193b8ef8a331033ed04a84fddc835aa8799
John Hamelink <hello@farmer.io>
2014-10-27 23:49:29 +0000

    Replaced a tonne of different things with unite.vim
    Added systemd support for zsh

--- zsh/Linux.sh
+++ zsh/Linux.sh
@@ -1,10 +1,12 @@
 antigen bundle archlinux
 antigen bundle fasd
+antigen bundle systemd
 
 alias pacman="pacaur"
 alias gksu="gksudo"
 alias open="xdg-open"
 export BROWSER="chromium"
+alias term="terminology &> /dev/null"
 
 whiteboardify (){
     convert "$1" -morphology Convolve DoG:15,100,0 -negate -normalize -blur 3x1 -channel RBG -level 60%,91%,0.1 "$2"
